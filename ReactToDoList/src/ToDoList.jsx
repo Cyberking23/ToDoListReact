73f871fea550621debaf32c832657723c723462c
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-09-16 10:15:20 -0600

    React To Do List
    asdfasdfdsaf

--- ReactToDoList/src/ToDoList.jsx
+++ ReactToDoList/src/ToDoList.jsx
@@ -1,41 +1,71 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function ToDoList() {
-    const [tasks,setTasks]= useState(["Eat Breakfast","Task a shower"]);
-    const [newTask,setNewTask]=useState("");
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-    function handleInputChange(event){
-        setNewTask(event.target.value)
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+  function addTask() {
+    if(newTask.trim()!== ""){
+        setTasks(t=>[...t,newTask])
+        newTask("")
+    }else{
+        alert("No se puede poner campo en blanco")
     }
-    function addTask(){
+  }
+  function deleteTask(index) {
+    const updatedTasks= tasks.filter((element,i)=> i !== index)
+    setTasks(updatedTasks);
 
+  }
+  function moveTaskUp(index) {
+    if(index>0){
+        const updatedTasks = [...tasks];
+        [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1],updatedTasks[index]]
+        setTasks( updatedTasks);
     }
-    function deleteTask(index){
+  }
+  function moveTaskDown(index) {
+     const updatedTasks = [...tasks];
+     [updatedTasks[index], updatedTasks[index + 1]] = [
+       updatedTasks[index + 1],
+       updatedTasks[index],
+     ];
+     setTasks(updatedTasks);
+  }
 
-    }
-    function moveTaskUp(index){
-
-    }
-    function moveTaskDown(index){
-
-    }
-    
-    return (
-    <div className='to-do-list'>
-        <h1>To-do-list</h1>
-        <div>
-            <input type="text" placeholder='Enter a task...' value={newTask} onChange={handleInputChange} />
-            <button className='add-button' onClick={addTask}>
-                Add
+  return (
+    <div className="to-do-list">
+      <h1>To-do-list</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
             </button>
-        </div>
-        <ol>
-            {tasks.map((task,index)=>
-                <li key={index}>
-                    <span className='text'>{task}</span>
-                </li>
-            )}
-        </ol>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              UP
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              Down
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
-  )
+  );
 }
